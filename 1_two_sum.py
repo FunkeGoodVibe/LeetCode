2c13eebbdb78a83ae547efb1a5c14a81c595ca40
Funke Sowole <funkegoodvibe@gmail.com>
2026-06-03 09:41:48 +0100

    add design

--- 1_two_sum.py
+++ 1_two_sum.py
@@ -4,7 +4,6 @@ Given an array of integers nums and an integer target, return indices of the two
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
 You can return the answer in any order.
 
- 
 
 Example 1:
 
@@ -33,10 +32,12 @@ Follow-up: Can you come up with an algorithm that is less than O(n2) time comple
 
 """
 
+
 from typing import List
 
 
 class Solution:
+
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         # Store each number we have already visited and its index.
         seen = {}
